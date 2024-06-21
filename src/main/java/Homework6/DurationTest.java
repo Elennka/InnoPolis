da135ae8673ea79ebae9cfe52577b698187bff33
Elennka <vean125@mail.ru>
2024-06-21 12:00:04 +0500

    Homework6 duration

--- src/main/java/Homework6/DurationTest.java
+++ src/main/java/Homework6/DurationTest.java
@@ -5,7 +5,7 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class DurationTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
 
 
         LocalDateTime currentDateTime = LocalDateTime.now();
